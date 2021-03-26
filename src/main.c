1516459121871db9c378021ff894ac7e6f8ccdd7
j0hnbl4k3 <pandeyjatin47@gmail.com>
2021-03-27 03:24:50 +0530

    change shell name to dish

--- src/main.c
+++ src/main.c
@@ -254,7 +254,7 @@ void lsh_loop(void)
   int status;
 
   do {
-    printf("> ");
+    printf("dish-> ");
     line = lsh_read_line();
     args = lsh_split_line(line);
     status = lsh_execute(args);
